aa17c4448ab094a5c8f81a425f13d295c7eb8298
Jordan Brasko <jbrasko0508@gmail.com>
2020-10-16 21:43:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 // write your code below!
-function happyHolidays(string) {
-  var string = ("Happy holidays!");
+function happyHolidays() {
   return string
 }
-function happyHolidaysTo(string) {
-var hh = (`Happy holidays, ${name}!`)
-return hh
-}
+
